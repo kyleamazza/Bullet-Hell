65cb3efd88a5228db75a0bc194f80aa4cbdee441
kylemazza <kyleamazza@gmail.com>
2018-09-08 21:19:14 -0700

    EnemySpawner, WaveSpawner, and Wave configuration scripts

---
+++ Assets/_Scripts/WaveConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Wave Config")]
+public class WaveConfig : ScriptableObject {
+
+    [SerializeField] AnimationClip pathAnim;
+    [SerializeField] RuntimeAnimatorController pathAnimController;
+    //[SerializeField] int numberOfEnemies;
+    [SerializeField] int numberOfEnemies;
+    [SerializeField] float spawnDelayTimer;
+    [SerializeField] float movementSpeed;
+    // TODO: bullet pattern/bullet emitter w/ pattern
+
+    public RuntimeAnimatorController PathAnimController
+    {
+        get { return pathAnimController; }
+    }
+
+    public AnimationClip PathAnim
+    {
+        get { return pathAnim; }
+    }
+
+	public int NumberOfEnemies
+    {
+        get { return numberOfEnemies; }
+    }
+
+    public float SpawnDelayTimer
+    {
+        get { return spawnDelayTimer; }
+    }
+
+    public float MovementSpeed
+    {
+        get { return movementSpeed; }
+    }
+}
---
+++ Assets/_Scripts/WaveSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    [SerializeField] List<WaveConfig> waveConfigs;
+    [SerializeField] int startingWave = 0;
+    ObjectPooler enemyPool;
+
+	// Use this for initialization
+	IEnumerator Start () {
+        enemyPool = ObjectPooler.instance;
+        yield return StartCoroutine(SpawnAllWaves());
+	}
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for (int i = startingWave; i < waveConfigs.Count; i++)
+        {
+            WaveConfig currentWave = waveConfigs[i];
+            yield return StartCoroutine(SpawnWave(currentWave));
+        }
+    }
+	
+	private IEnumerator SpawnWave(WaveConfig waveConfig)
+    {
+        int numberOfEnemies = waveConfig.NumberOfEnemies;
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            GameObject obj = enemyPool.GetPooledObject("Enemy");
+            if (obj != null)
+            {
+                Animator animator = obj.GetComponent<Animator>();
+                animator.runtimeAnimatorController = waveConfig.PathAnimController;
+                obj.SetActive(true);
+                yield return new WaitForSeconds(waveConfig.SpawnDelayTimer);
+            }
+            yield return null;
+        }
+    }
+}
