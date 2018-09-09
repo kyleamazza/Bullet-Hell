b4a61e70b64d5ec23a8a9a3a80e781dd8e3b6e21
kylemazza <kyleamazza@gmail.com>
2018-09-08 21:17:22 -0700

    Adding generic pooler scripts

---
+++ Assets/_Scripts/ObjectPooler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour {
+
+    public static ObjectPooler instance;
+    [SerializeField] List<ObjectPoolItem> itemsToPool;
+    List<GameObject> pooledObjects;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        foreach (ObjectPoolItem poolObject in itemsToPool) {
+            for (int i = 0; i < poolObject.pooledAmount; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(poolObject.objectToPool);
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+            }
+        }
+	}
+
+    public GameObject GetPooledObject(string tag)
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy && pooledObjects[i].tag == tag)
+            {
+                return pooledObjects[i];
+            }
+        }
+
+        foreach (ObjectPoolItem poolObject in itemsToPool)
+        {
+            if (poolObject.tag == tag && poolObject.canGrow)
+            {
+                GameObject obj = (GameObject)Instantiate(poolObject.objectToPool);
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+                return obj;
+            }
+        }
+
+        return null;
+    }
+}
