94c905c1b0fb233c97e77b3eb22ee0cba9fbc523
kylemazza <kyleamazza@gmail.com>
2018-09-08 21:16:35 -0700

    Adding stub Enemy.cs script

---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] int health = 500;
+	
+	// Update is called once per frame
+	void Update () {
+        // Hit detection
+	}
+}
